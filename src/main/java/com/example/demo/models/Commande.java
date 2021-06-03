9567c5e4d7e04309a686ada63d4cd0fd06093730
SamyMercier <56022459+SamyMercier@users.noreply.github.com>
2021-06-03 15:53:44 +0200

    ajouts repertoires et finition des models

--- src/main/java/com/example/demo/models/Commande.java
+++ src/main/java/com/example/demo/models/Commande.java
@@ -1,5 +1,53 @@
 package com.example.demo.models;
 
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Commande {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "client_id", referencedColumnName = "id")
+	private Client client;
+
+	@ManyToMany
+	@JoinTable(
+			name = "commande_has_produit",
+			joinColumns = @JoinColumn(name = "commande_id"),
+			inverseJoinColumns = @JoinColumn(name = "produit_id")
+			)
+	private Collection<Produit> produits;
+	
+	public Commande() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
 
 }
--- src/main/java/com/example/demo/models/Produit.java
+++ src/main/java/com/example/demo/models/Produit.java
@@ -1,5 +1,54 @@
 package com.example.demo.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Produit {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "nom")
+	private String nom;
+	@Column(name = "prix")
+	private double prix;
+	
+	@ManyToOne
+	@JoinColumn(name = "categorie_id", referencedColumnName = "id")
+	private Categorie categorie;
+	
+	public Produit() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
 
 }
